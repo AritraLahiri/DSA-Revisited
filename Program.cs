c7b6f40b919acee16d4630c5d7a780f275682ae8
Aritra <aritralahiri17@gmail.com>
2025-02-22 13:21:32 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using DSA_Revisted;
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+        int ans = Sum_N_Natural_Numbers.FindNNaturalNumbers(10);
+        Console.WriteLine(ans);
+
+    }
+
+}
---
+++ Sum_N_Natural_Numbers.cs
@@ -0,0 +1,27 @@
+﻿namespace DSA_Revisted
+{
+    internal static class Sum_N_Natural_Numbers
+    {
+        public static int FindNNaturalNumbers(int n)
+        {
+
+            //O(N) Approach
+            //if (n == 0) return 0;
+            //int res = 0;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    res += i;
+            //}
+            //return res;
+
+            // Most Efficient O(1) 
+            return n * (n + 1) / 2;
+
+        }
+
+    }
+}
+
+
+
+
